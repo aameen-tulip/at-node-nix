a605aead8ac6fc0d7137bcc7580980d2eb8e8174
Alex Ameen <alex.ameen@tulip.co>
2022-05-24 17:43:18 -0500

    expanded link-farm example

--- ref/link-farm/default.nix
+++ ref/link-farm/default.nix
@@ -14,12 +14,30 @@ let
       "version": "1.0.0"
     }' > $out/lib/node_modules/@${scope}/${pname}/package.json
   '';
+
+  linkyModule = runCommandNoCC "linky-module" {
+    m1 = mkModule "hey" "there";
+    m2 = mkModule "hey" "dude";
+    preferLocalBuild = true;
+    allowSubstitutes = false;
+  } ''
+    mkdir -p $out/lib/node_modules/linky/node_modules
+    echo '\
+    { "name": "linky",
+      "version": "1.0.0"
+    }' > $out/lib/node_modules/linky/package.json
+    ${lndir}/bin/lndir -silent -ignorelinks $m1/lib/node_modules $out/lib/node_modules/linky/node_modules
+    ${lndir}/bin/lndir -silent -ignorelinks $m2/lib/node_modules $out/lib/node_modules/linky/node_modules
+  '';
+
   modules = [
     ( mkModule "foo" "bar" )
     ( mkModule "foo" "sally" )
     ( mkModule "baz" "quux" )
     ( mkModule "petr" "kropotkin" )
+    linkyModule
   ];
+
 in runCommandNoCC "my-node_modules" {
      inherit modules;
      preferLocalBuild = true;
