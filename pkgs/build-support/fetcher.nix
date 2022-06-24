db43ac09e726a0c2e8e4a36bb1c137ba54c1bf21
Alex Ameen <alex.ameen@tulip.co>
2022-06-24 15:29:35 -0500

    typo

--- pkgs/build-support/fetcher.nix
+++ pkgs/build-support/fetcher.nix
@@ -13,7 +13,7 @@
   plockEntryHashAttr = entry: let
     integrity2Sha = integrity: let
       m = builtins.match "(sha(512|256|1))-(.*)" integrity;
-      shaSet = { ${builtins.head m} = ${builtins.elemAt m 2}; };
+      shaSet = { ${builtins.head m} = builtins.elemAt m 2; };
     in if m == null then { hash = integrity; } else shaSet;
     fromInteg = integrity2Sha entry.integrity;
   in if entry ? integrity then fromInteg else
