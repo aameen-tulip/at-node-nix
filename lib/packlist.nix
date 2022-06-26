2c01092355c36164125d9a75530d1703918fe349
Alex Ameen <alex.ameen.tx@gmail.com>
2022-06-26 16:22:25 -0500

    packlist

--- lib/packlist.nix
+++ lib/packlist.nix
@@ -1,9 +1,28 @@
 { lib }: let
 
-  # Javascript's RE is not case sensitive, so we need to handle things like
+  inherit (builtins)
+    attrNames
+    concatLists
+    concatStringsSep
+    elem
+    elemAt
+    filter
+    head
+    isString
+    match
+    replaceStrings
+    split
+    stringLength
+    substring
+  ;
+
+
+/* --------------------------------------------------------------------------- */
+
+  # `ignore-walk' REXPs are not case sensitive, so we need to handle things like
   # "rEaDmE" explicitly.
   packageMustHaveFileNames' = "readme|copying|license|licence";
-  packageMustHaveFileNames = builtins.concatStringsSep "|" [
+  packageMustHaveFileNames = concatStringsSep "|" [
     "[Rr][Ee][Aa][Dd][Mm][Ee]"
     "[Cc][Oo][Pp][Yy][Ii][Nn][Gg]"
     "[Ll][Ii][Cc][Ee][Nn][SsCc][Ee]"
@@ -53,6 +72,9 @@
     "/archived-packages/**"
   ];
 
+
+/* --------------------------------------------------------------------------- */
+
   # `npm-packlist' implements a tree walker that goes to search for bundled
   # dependencies which should be packed.
   # Notably, it considers symlinked members of the `node_modules/' directory
@@ -67,7 +89,7 @@
     browser = if pjs ? browser then ["/${pjs.browser}"] else [];
     main = if pjs ? main then ["/${pjs.main}"] else [];
     bins =
-      map ( k: "/${pjs.bin.${k}}" ) ( builtins.attrNames ( pjs.bin or {} ) );
+      map ( k: "/${pjs.bin.${k}}" ) ( attrNames ( pjs.bin or {} ) );
     defaults = [
       "/package.json"
       "/npm-shrinkwrap.json"
@@ -76,44 +98,73 @@
     ];
   in defaults ++ browser ++ main ++ bins;
 
-  # Rules can a multiline string, list of single/multiline strings.
-  # We split these into single lines, so we prefer to receive that.
-  onReadIgnoreFile = file: rules: let
-    # Split rules to trimmed lines.
-    srules = let
-      inherit (builtins) filter isString split concatLists;
-      splitAndTrim = b: filter isString ( split "[ \t]*\r?\n[ \t]*" b );
-      s = if isString rules then splitAndTrim rules else
-          concatLists ( map splitAndTrim rules );
-    in filter ( l: ( l != "" ) && ( ! ( lib.test "^#" l ) ) ) s;
 
-    # Whatever a "minimatch" is. I'm guessing a little regex compiler in JS?
-    # they pass the rules patterns with `mmopt' to filter matches.
-    mmopt = {
-      matchBase  = true;  # Match ~~basename~~ ( they mean the fucking list suffix on "/", but they're webshits so "foo/bar" is a basename of "baz/foo/bar" so far as they can tell... Do you think they tie their own shoes? )
-      dot        = true;  # Match hidden files
-      flipNegate = true;  # Inverts the meaning of "!foo" to "keep foo"
-      nocase     = true;  # Case insensitive... which what the fuck? seriously? Git does not do this, so they're misinterpreting every `.gitignore' in every project.
-    };
-    mkMatcher = rule: let
-    in null;
-  in {};
+/* --------------------------------------------------------------------------- */
+
+  # `rules' is a string, with the contents of a `.{npm,git}ignore' file, or
+  # a path to an ignore file be parsed.
+  # Lines from `package.json' `files: []' field may also be passed in, just
+  # join them to a multiline string first.
+  #
+  # Single line strings are fine; but it makes a bit more sense to join them
+  # before calling this.
+  # We use `hercules-ci/gitignore.nix' to convert the `.gitignore' patterns to
+  # PCRE, but that could easily be done here if we wanted to drop
+  # the dependency.
+  # FIXME: fork and modify `hercules-ci/gitignore.nix/rules.nix' and allow
+  # a list of strings to be passed in - we waste a bit of effort joining, just
+  # to have `gitignoreToRegexes' split it immediately after.
+  #
+  # Returns a list of tuples: [["^[^/]*" false] ["(^|.*/)bin($|/.*)" true]]
+  # where `false' indicates that the file should be ignored, and `true' causes
+  # the file to be packaged in the tarball.
+  # This list is meant to be run later by `libgi.filterPattern' which allows a
+  # prefix to be provided which should match directory that the ignores were
+  # defined in - this is used to organize inheritance of ignores, and we do not
+  # handle that here.
+  # XXX: These patterns are case sensitive! NPM's are not, but their approach is
+  # total ass because 99% of users don't expect their `.gitignore' to be
+  # reinterpreted this way - it was a poor design decision and we don't
+  # replicate it.
+  #
+  # We use case sensitive matching, and have explicitly created the REGEXPs
+  # for the `packageMustHaveFilenames' so that things like "README.md" will be
+  # matched, but unlike NPM, we don't treat all patterns as case insensitive.
+  # This shouldn't cause issues on projects that were created by sane
+  # developers; and since that behavior largely exists to support Micro$oft -
+  # we really only have to look out for projects that were authored by shills.
+  #
+  # NOTE: The `ignore-walk' implementation `onReadIgnoreFile' takes `file' as an
+  # argument to stash the results of this operation in a hash-map cache.This
+  # implementation leaves that sort of thing up to the caller.
+  readIgnoreFile = x: let
+    rules = if isString x then x else builtins.readFile x;
+  in lib.libgi.gitignoreToRegexes ni;
+
+
+/* --------------------------------------------------------------------------- */
 
   # Entries is a list of paths to projects.
   # We use relative paths, which makes processing ignore patterns much simpler.
   onReadDir = cwd: entries: let
   in {};
 
+
+/* --------------------------------------------------------------------------- */
+
   getPackageFiles = cwd: entries: pjs: let
     fromDir = onReadDir cwd entries;
     mustPjs = mustHaveFrilesFromPackage pjs;
     doBundle = ( pjs.bundleDependencies or false ) ||
                ( pjs.bundledDependencies or false );
-    maybeNm = if doBundle && ( builtins.elem "node_modules" entries ) then
+    maybeNm = if doBundle && ( elem "node_modules" entries ) then
               ["node_modules"] else [];
     # FIXME: process `files: []' as gitignore patterns
   in {
   };
+
+
+/* --------------------------------------------------------------------------- */
 
   # This is an equivalent to the `npm-packlist'/`ignore-walk' packages class
   # `class Walker : extends IgnoreWalker'.
@@ -185,7 +236,7 @@
     # This is placed here to avoid putting this big ugly block above in the
     # fallback - but this was just a stylistic decisions.
     bundledScopes' = let
-      scoped = builtins.filter ( lib.hasPrefix "@" ) bundled;
+      scoped = filter ( lib.hasPrefix "@" ) bundled;
     in if bundledScopes != null then bundledScopes else
        map ( lib.yank' "(@[^/]+)/.*" ) scoped;
     # This sub-condition of `isProject' handles workspaces "inheritence" of
@@ -198,6 +249,9 @@
     childig     = map ( lib.libpath.realpathRel' opt.path ) opt.workspaces;
     wsIgnores   = if isWsContext then rootig else childig;
     # XXX: now they invoke `super.onReadIgnoreFile'
+
+
+/* --------------------------------------------------------------------------- */
 
   in {};
 
