b78122dd634382edf93322dbdee3912d7b9feb6d
Alex Ameen <alex.ameen@tulip.co>
2022-07-01 14:42:37 -0500

    update locks

--- pkgs/build-support/buildGyp.nix
+++ pkgs/build-support/buildGyp.nix
@@ -7,6 +7,15 @@
 
 /* -------------------------------------------------------------------------- */
 
+  withHooks = type: body: let
+    up = let
+      u = lib.toUpper ( builtins.substring 0 1 type );
+    in u + ( builtins.substring 1 ( builtins.stringLength type ) type );
+  in "runHook pre${up}\n${body}\nrunHook post${up}";
+
+
+/* -------------------------------------------------------------------------- */
+
   buildGyp = {
     name         ? "node-gyp-build"
   , src
