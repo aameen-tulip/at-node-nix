f0f5674f54ceda3bf072b3b6c5c1c38f657c890f
Alex Ameen <alex.ameen@tulip.co>
2022-07-09 15:01:01 -0500

    accept depfields in plock2nmFocus'

--- pkgs/build-support/plock-to-node-modules-dir.nix
+++ pkgs/build-support/plock-to-node-modules-dir.nix
@@ -10,16 +10,19 @@
     unpacked = attrs.unpacked or tb.unpacked;
   in { name = key; path = unpacked.outPath or unpacked; };
 
-  plock2nmFocus = plock: workspace: let
+  plock2nmFocus' = depFields: plock: workspace: let
     inherit (lib.libplock) depClosureFor;
-    deps  = depClosureFor ["dependencies" "devDependencies"] plock workspace;
+    deps  = depClosureFor depFields plock workspace;
     deps' = builtins.filter ( x: x.key != workspace ) deps;
   in linkModules { modules = ( map ftpath deps' ); };
+
+  # FIXME: peerDeps
+  plock2nmFocus = plock2nmFocus' ["dependencies" "devDependencies"];
 
   plock2nm = plock:
     linkModules { modules = ( map ftpath plock.packages ); };
 
 in {
   plockEntryFetchUnpack = ftpath;
-  inherit plock2nmFocus plock2nm;
+  inherit plock2nmFocus' plock2nmFocus plock2nm;
 }
