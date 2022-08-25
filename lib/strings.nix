c7ae85c70aadd55fa7c448ec4d0c9f30edfd68c7
aameen-tulip <98349328+aameen-tulip@users.noreply.github.com>
2022-08-25 02:33:42 -0500

    maint: delete a large amount of dead code (Part 1)
    - maint: delete a large amount of dead code. ( TODO: build-support )
    - libplock: ironically code had to be added for other files to be deleted. TODO: This needs to be pruned.

--- lib/strings.nix
+++ lib/strings.nix
@@ -1,19 +1,22 @@
+# ============================================================================ #
+
 { lib }: let
 
-  lines = lib.splitString "\n";
+# ---------------------------------------------------------------------------- #
 
   # `ak-nix.lib' also carries a `libstr' which we have to refer to here.
   applyToLines = f: x: let
     inherit (builtins) isString isPath isList concatStringsSep readFile;
+    _lines = str: builtins.filter builtins.isString ( builtins.split "\n" str );
     asList = if ( isList x ) then x
-      else if ( isString x ) then lines x
+      else if ( isString x ) then _lines x
       else if ( isPath x )   then lib.readLines x
       else throw ( "Cannot convert type ${builtins.typeOf x} to a list" +
                     " of strings" );
   in lib.concatMapStringsSep "\n" f asList;
 
 
-/* -------------------------------------------------------------------------- */
+# ---------------------------------------------------------------------------- #
 
   removeSlashSlashComment' = line:
     let ms = builtins.match "([^\"]*(\"[^\"]*\")*[^\"]*[^\\\"])//.*" line;
@@ -27,28 +30,18 @@
   removePoundComments = applyToLines removePoundComment';
 
 
-/* -------------------------------------------------------------------------- */
-
-  trim = str: let
-    ws = "[ \t\n\r]";
-    pr = "[^ \t\n\r]";
-  in builtins.head ( builtins.match "${ws}*(${pr}+)${ws}*" str );
-
-
-/* -------------------------------------------------------------------------- */
-
-  # FIXME: move to pkginfo or parse?
-  renamePkgAtVersion = n: let
-    m = builtins.match "@([^/]+)/([^@]+)@(.*)" n;
-    y = builtins.elemAt m;
-  in if ( builtins.substring 0 1 n ) == "@"
-     then "${y 0}--${y 1}--${y 2}"
-     else ( builtins.replaceStrings ["@"] ["--"] n );
-
-
-/* -------------------------------------------------------------------------- */
+# ---------------------------------------------------------------------------- #
 
 in {
-  inherit lines applyToLines trim;
-  inherit removeSlashSlashComments removePoundComments;
+  inherit
+    applyToLines
+    removeSlashSlashComments
+    removePoundComments
+  ;
 }
+
+# ---------------------------------------------------------------------------- #
+#
+#
+#
+# ============================================================================ #
