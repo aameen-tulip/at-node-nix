7b6f238f7779f57a7f31aa4e9664e7c460d6c83d
Alex Ameen <alex.ameen@tulip.co>
2022-10-01 02:29:12 -0500

    update pacote flake

--- pkgs/development/node-packages/pacote/flake.nix
+++ pkgs/development/node-packages/pacote/flake.nix
@@ -1,26 +1,31 @@
 {
   description = "npm fetcher utility";
 
-  inputs.utils.url = "github:numtide/flake-utils";
-  inputs.utils.inputs.nixpkgs.follows = "/nixpkgs";
   inputs.pacote-src.url = "github:npm/pacote/v13.3.0";
   inputs.pacote-src.flake = false;
 
-  outputs = { self, nixpkgs, utils, pacote-src }:
-    utils.lib.eachDefaultSystem ( system: let
-      pkgs = nixpkgs.legacyPackages.${system};
-      inherit ( import ./default.nix { inherit pkgs; src = pacote-src; } )
-        sources package shell nodeDependencies;
-      pacote = package;
-      app = utils.lib.mkApp { drv = package; exePath = "/bin/pacote"; };
-      overlays = final: prev: { pacote = package; };
+  outputs = { self, nixpkgs, utils, pacote-src }: {
+    overlays.pacote = final: prev: let
+      pacoteFull = nixpkgs.lib.callPackageWith final ./. {
+        nodejs = final.nodejs-14_x;
+        src = pacote-src;
+      };
     in {
-      packages.pacote = pacote;
-      defaultPackage = pacote;
-      apps.pacote = app;
-      defaultApp = app;
-      nodeDependencies = nodeDependencies;
-      nodeShell = shell;
-      inherit overlays;
-    } );
+      pacote = pacoteFull.package // {
+        # Passthru carries: sources shell nodeDependencies
+        passthru = removeAttrs pacoteFull ["package"];
+      };
+    };
+    packages = builtins.foldl' ( acc: system: let
+      pkgsFor = nixpkgs.legacyPackages.${system}.extend self.overlays.pacote;
+    in acc // {
+      ${system} = {
+        inherit (pkgsFor) pacote;
+        default = self.packages.${system}.pacote;
+      };
+    } ) {} [
+      "x86_64-linux" "aarch64-linux" "i686-linux"
+      "x86_64-darwin" "aarch64-darwin"
+    ];
+  };
 }
