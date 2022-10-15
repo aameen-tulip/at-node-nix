e8880c1646b1d393feb776e800fe5e74b942a6d3
Alex Ameen <alex.ameen@tulip.co>
2022-10-15 13:37:39 -0500

    tests NpmLock type 'packages'

--- tests/types/tests.nix
+++ tests/types/tests.nix
@@ -20,8 +20,19 @@
   inherit (yt.Packument.Structs)
     packument
   ;
+  inherit (yt.NpmLock.Strings)
+    relative_file_uri
+    git_uri
+    tarball_uri
+    resolved_uri
+  ;
   inherit (yt.NpmLock.Structs)
+    pkg_path
+    pkg_dir
+    pkg_link
     pkg_tarball
+    pkg_git
+    package
   ;
 
   inherit ( import ../data/plocks.nix )
@@ -60,27 +71,110 @@
     in if lib.inPureEvalMode then {} else builtins.foldl' proc {} packs;
 
     # The `@floco/arbor' lock only uses registry tarballs.
-    plTarballEnts = let
+    plockEnts = let
       pkgs = let
-        named = lib.libattrs.pushDownNames ( removeAttrs arb3.packages [""] );
+        named = lib.libattrs.pushDownNames arb3.packages;
         vals  = builtins.attrValues named;
       in if limit < 1000 then lib.take limit vals else vals;
-
       proc = acc: p: let
-        ent = removeAttrs p ["name"];
-      in acc // {
+        tname = if p.name == "" then "ROOT" else p.name;
+        ent = if p.name == "" then p // { inherit (arb3.packages."") name; }
+                              else removeAttrs p ["name"];
+        # We skip the root entry on this one.
+        tb = lib.optionalAttrs ( p.name != "" ) {
           "testPlockEntryTarball_${p.name}" = {
             expr     = pkg_tarball ent;
             expected = ent;
           };
         };
+      in acc // tb // {
+          "testPlockEntryPackage_${tname}" = {
+            expr     = package ent;
+            expected = ent;
+          };
+      };
     in builtins.foldl' proc {} pkgs;
 
 # ---------------------------------------------------------------------------- #
 
-  in npmTop1000 // plTarballEnts // {
+  in npmTop1000 // plockEnts // {
 
     inherit packs arb2 arb3 lib yt;
+
+# ---------------------------------------------------------------------------- #
+
+    testPkgPath_0 = {
+      expr     = pkg_path.check { name = "foo"; version = "1.0.0"; };
+      expected = true;
+    };
+
+    testPkgPath_1 = {
+      expr = pkg_path.check {
+        version  = "1.0.0";
+        link     = true;
+        resolved = "../foo";
+      };
+      expected = true;
+    };
+
+    testPkgPath_2 = {
+      expr = pkg_path.check {
+        version      = "1.0.0";
+        dependencies = { bar = "^1.0.0"; };
+      };
+      expected = true;
+    };
+
+
+# ---------------------------------------------------------------------------- #
+
+    testPkgDir_0 = {
+      expr     = pkg_dir.check { name = "foo"; version = "1.0.0"; };
+      expected = true;
+    };
+
+    testPkgDir_1 = {
+      expr = pkg_dir.check {
+        version  = "1.0.0";
+        link     = true;
+        resolved = "../foo";
+      };
+      expected = false;
+    };
+
+    testPkgDir_2 = {
+      expr = pkg_dir.check {
+        version      = "1.0.0";
+        dependencies = { bar = "^1.0.0"; };
+      };
+      expected = true;
+    };
+
+
+# ---------------------------------------------------------------------------- #
+
+    testPkgLink_0 = {
+      expr     = pkg_link.check { name = "foo"; version = "1.0.0"; };
+      expected = false;
+    };
+
+    testPkgLink_1 = {
+      expr = pkg_link.check {
+        version  = "1.0.0";
+        link     = true;
+        resolved = "../foo";
+      };
+      expected = true;
+    };
+
+    testPkgLink_2 = {
+      expr = pkg_link.check {
+        version      = "1.0.0";
+        dependencies = { bar = "^1.0.0"; };
+      };
+      expected = false;
+    };
+
 
 # ---------------------------------------------------------------------------- #
 
