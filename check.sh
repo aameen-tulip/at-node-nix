f7f6d2d3c42fad1e14ccdc4730877bbcc1dc168d
Alex Ameen <alex.ameen@tulip.co>
2022-10-28 18:54:07 -0500

    doc pacote builder

--- check.sh
+++ check.sh
@@ -5,10 +5,12 @@ set -eu;
 : "${NIX_FLAGS:=-L --show-trace}";
 : "${SYSTEM:=$( $NIX eval --raw --impure --expr builtins.currentSystem; )}";
 
+trap '_es="$?"; exit "$_es";' HUP TERM EXIT INT QUIT;
+
 $NIX flake check $NIX_FLAGS;
 $NIX flake check $NIX_FLAGS --impure;
 $NIX flake check $NIX_FLAGS --system "$SYSTEM";
 $NIX flake check $NIX_FLAGS --system "$SYSTEM" --impure;
-trap '_es="$?"; rm -f ./result; exit "$_es";' HUP TERM EXIT INT QUIT;
+NIX_FLAGS+=' --no-link';
 $NIX build .#tests $NIX_FLAGS;
 $NIX build .#tests $NIX_FLAGS --impure;
