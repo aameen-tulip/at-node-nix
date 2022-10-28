b0b12ac02e7b6a8d954ab04cf029364088e7ca34
aameen-tulip <98349328+aameen-tulip@users.noreply.github.com>
2022-10-28 14:52:14 -0500

    inject dependencies as variable names into 'pjsUtil' setup hook (#24)

--- pkgs/build-support/setup-hooks/pjs-util.nix
+++ pkgs/build-support/setup-hooks/pjs-util.nix
@@ -1,5 +1,21 @@
-# Also: coreutils findutils gnused
-{ makeSetupHook, jq, bash, nodejs }: makeSetupHook {
+{ makeSetupHook
+, jq, bash, nodejs, gnugrep, gnused, findutils, coreutils
+}: makeSetupHook {
   name = "pjs-util";
-  deps = [jq bash nodejs];
+  deps = [jq bash nodejs gnugrep gnused findutils coreutils];
+  substitutions.mk_setup_hook_inject = ''
+     These dependencies have been injected by `makeSetupHook'.
+    : "''${CP:=${coreutils}/bin/cp}";
+    : "''${LN:=${coreutils}/bin/ln}";
+    : "''${MKDIR:=${coreutils}/bin/mkdir}";
+    : "''${CHMOD:=${coreutils}/bin/chmod}";
+    : "''${READLINK:=${coreutils}/bin/readlink}";
+    : "''${REALPATH:=${coreutils}/bin/realpath}";
+    : "''${JQ:=${jq}/bin/jq}";
+    : "''${SED:=${gnused}/bin/sed}";
+    : "''${GREP:=${gnugrep}/bin/grep}";
+    : "''${FIND:=${findutils}/bin/find}";
+    : "''${BASH:=${bash}/bin/bash}";
+  '';
+  meta = { inherit (bash.meta) platforms; };
 } ./pjs-util.sh
