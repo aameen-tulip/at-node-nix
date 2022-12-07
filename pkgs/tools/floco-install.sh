86b2f7db634b1ab57f1fca766e7565217c8ab2fb
aameen-tulip <98349328+aameen-tulip@users.noreply.github.com>
2022-12-07 02:29:12 -0600

    Create floco-install.sh

---
+++ pkgs/tools/floco-install.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+while [[ "$#" -gt 0 ]]; do
+  case "$1" in
+    --prefix=*)  PREFIX="${1#*=}"; ;;
+    --prefix)    shift; PREFIX="$1"; ;;
+    --bindir=*)  BINDIR="${1#*=}"; ;;
+    --bindir)    shift; BINDIR="$1"; ;;
+    --libdir=*)  LIBDIR="${1#*=}"; ;;
+    --libdir)    shift; LIBDIR="$1"; ;;
+    -g|--global) GLOBAL=1; ;;
+    -l|--local)  LOCAL=1; ;;
+    --node=*)    NODE="${1#*=}"; ;;
+    --node)      shift; NODE="$1"; ;;
+    -l|--lndirs) STYLE=lndirs; ;;
+    -c|--copy)   STYLEcopy; ;;
+    --bin-links) BSTYLE=link; ;;
+    --bin-wraps) BSTYLE=wrap; ;;
+    --no-bins)   BSTYLE=skip; ;;
+    --mode=*)    MODE="${1#*=}"; ;;
+    --mode)      shift; MODE="$1"; ;;
+  esac
+  shift;
+done
